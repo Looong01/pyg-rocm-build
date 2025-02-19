9b2212810dc0a6b6aa5d3313a897b693bdcc6d02
Looong01 <lizelongdd@hotmail.com>
2025-02-19 01:18:02 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -44,12 +44,10 @@ versions=("py39" "py310" "py311" "py312" "py313")
 
 for version in "${versions[@]}"; do
     mkdir -p ${current_path}/dist/${package_name}-${version}-linux_x86_64
-    
+    py_version="${version#py}"
     for module in "pytorch_cluster" "pytorch_scatter" "pytorch_sparse" "pytorch_spline_conv"; do
-        mv ${current_path}/${module}/dist/*-cp${version: -2}-cp${version: -2}-linux_x86_64.whl ${current_path}/dist/${package_name}-${version}-linux_x86_64/
+        mv "${current_path}/${module}/dist/"*-cp${py_version}-cp${py_version}-linux_x86_64.whl "${current_path}/dist/${package_name}-${version}-linux_x86_64/"
     done
-
-    zip ${current_path}/dist/${package_name}-${version}-linux_x86_64.zip ${current_path}/dist/${package_name}-${version}-linux_x86_64/*.whl
 done
 
 for module in "pytorch_cluster" "pytorch_scatter" "pytorch_sparse" "pytorch_spline_conv"; do
@@ -62,7 +60,7 @@ wget "https://files.pythonhosted.org/packages/03/9f/157e913626c1acfb3b19ce000b1a
 
 for version in "${versions[@]}"; do
     cp ${current_path}/dist/torch_geometric-2.6.1-py3-none-any.whl ${current_path}/dist/${package_name}-${version}-linux_x86_64/
-    zip ${current_path}/dist/${package_name}-${version}-linux_x86_64.zip ${current_path}/dist/${package_name}-${version}-linux_x86_64/*.whl
+    zip -j ${current_path}/dist/${package_name}-${version}-linux_x86_64.zip ${current_path}/dist/${package_name}-${version}-linux_x86_64/*.whl
 done
 
 rm ${current_path}/dist/torch_geometric-2.6.1-py3-none-any.whl
